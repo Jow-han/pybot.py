a422a4194e3befc611fd9882142d30f85214563a
kensixx <raiken67@gmail.com>
2022-02-25 16:06:46 +0800

    Added 1 Cog, added requirements.txt, updated README.md

---
+++ cogs/tanga.py
@@ -0,0 +1,16 @@
+import discord
+from discord.ext import commands
+
+class Tanga(commands.Cog):
+  
+  def __init__(self, bot):
+    self.bot = bot
+    
+  @commands.command() # In cogs, we use @commands.command() and not @bot.command().
+  async def tanga(self, ctx): # You must not forget to pass "self" as the first parameter for functions inside classes.
+    # We don't use "bot.something", we use "self.bot.something" in cogs.
+    await ctx.send(f"tanga mo") 
+    
+def setup(bot):
+  bot.add_cog(Tanga(bot))
+  print('Tanga is loaded!')
